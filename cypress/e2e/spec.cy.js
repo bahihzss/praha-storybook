b46451bb830400621d520905db4ba1ee45b20049
河本 勇人 <kawamoto.web@gmail.com>
2023-02-21 20:38:43 +0900

    ちょっと修正

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -18,10 +18,10 @@ describe('template spec', () => {
 
     cy.get('.game-board > :nth-child(1) > :nth-child(1) > :nth-child(1)').click();
     cy.get('.game-board > :nth-child(1) > :nth-child(1) > :nth-child(2)').click();
-    cy.get(':nth-child(1) > :nth-child(2) > :nth-child(3)').click();
+    cy.get('.game-board > :nth-child(1) > :nth-child(2) > :nth-child(3)').click();
     cy.get('.game-board > :nth-child(1) > :nth-child(2) > :nth-child(2)').click();
-    cy.get(':nth-child(1) > :nth-child(3) > :nth-child(1)').click();
-    cy.get(':nth-child(3) > :nth-child(2)').click();
+    cy.get('.game-board > :nth-child(1) > :nth-child(3) > :nth-child(1)').click();
+    cy.get('.game-board > :nth-child(1) > :nth-child(3) > :nth-child(2)').click();
     cy.contains('Winner: O')
   })
 
