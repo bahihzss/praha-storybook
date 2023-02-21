a0b402c367bec4cf5e7e54e60638636a1cf84a8b
河本 勇人 <kawamoto.web@gmail.com>
2023-02-21 19:20:19 +0900

    data-e2e → data-testid

--- src/Square.jsx
+++ src/Square.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function Square(props) {
   return (
-    <button className="square" onClick={props.onClick} data-e2e="square">
+    <button className="square" onClick={props.onClick} data-testid="square">
       {props.value}
     </button>
   );
