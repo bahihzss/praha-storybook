cb8181bd09439c6ff01a7cc3d632df17f7f0d43d
河本 勇人 <kawamoto.web@gmail.com>
2023-02-21 19:20:38 +0900

    Game コンポーネントに引き分けのストーリーを追加

--- src/Game.stories.js
+++ src/Game.stories.js
@@ -1,7 +1,24 @@
 import { Game } from './Game'
+import { userEvent, within } from '@storybook/testing-library'
 
 export default {
   component: Game
 }
 
 export const Basic = {}
+
+export const Draw = {
+  async play({ canvasElement }) {
+    const canvas = within(canvasElement)
+    await userEvent.click(canvas.getAllByTestId('square')[0])
+    await userEvent.click(canvas.getAllByTestId('square')[1])
+    await userEvent.click(canvas.getAllByTestId('square')[2])
+    await userEvent.click(canvas.getAllByTestId('square')[4])
+    await userEvent.click(canvas.getAllByTestId('square')[3])
+    await userEvent.click(canvas.getAllByTestId('square')[5])
+    await userEvent.click(canvas.getAllByTestId('square')[7])
+    await userEvent.click(canvas.getAllByTestId('square')[6])
+    await userEvent.click(canvas.getAllByTestId('square')[8])
+
+  }
+}
