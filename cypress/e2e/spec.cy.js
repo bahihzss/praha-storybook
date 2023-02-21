7a3806d2a5f2f9b9cca765c4c17f2d3805e288ad
河本 勇人 <kawamoto.web@gmail.com>
2023-02-21 17:26:45 +0900

    引き分けのケースを追加

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -12,4 +12,20 @@ describe('template spec', () => {
 
     cy.contains('Winner: X')
   })
+
+  it('引き分け', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('[data-e2e="square"]').eq(0).click()
+    cy.get('[data-e2e="square"]').eq(1).click()
+    cy.get('[data-e2e="square"]').eq(2).click()
+    cy.get('[data-e2e="square"]').eq(4).click()
+    cy.get('[data-e2e="square"]').eq(3).click()
+    cy.get('[data-e2e="square"]').eq(5).click()
+    cy.get('[data-e2e="square"]').eq(7).click()
+    cy.get('[data-e2e="square"]').eq(6).click()
+    cy.get('[data-e2e="square"]').eq(8).click()
+
+    cy.contains('Draw!')
+  })
 })
--- src/Game.jsx
+++ src/Game.jsx
@@ -45,6 +45,7 @@ export class Game extends React.Component {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+    const isDraw = winner === null && current.squares.every(square => square !== null)
 
     const moves = history.map((step, move) => {
       const desc = move ?
@@ -60,6 +61,8 @@ export class Game extends React.Component {
     let status;
     if (winner) {
       status = "Winner: " + winner;
+    } else if (isDraw) {
+      status = "Draw!"
     } else {
       status = "次のプレイヤー: " + (this.state.xIsNext ? "X" : "O");
     }
@@ -92,8 +95,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6]
   ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+  for (const [a, b, c] of lines) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
